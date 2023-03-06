20f5c6d62b1698e382d511b1be0430a2792d3252
Noobwastaken99 <czmoni99@yahoo.com>
2023-03-06 14:27:07 -0600

    console log days to mars

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -3,11 +3,17 @@
 
 
 // Part 1: Declare (5) Variables With Type
-
-
+let spacecraftName: string = 'Determination';
+let speedMph: number = 17500;
+let kilomitersToMars: number = 225000000;
+let kilometersToTheMoon: number = 384400;
+let milesPerKilometer: number = 0.621;
+let milesToMars: number = kilomitersToMars * milesPerKilometer;
+let hoursToMars: number = milesToMars / speedMph;
+let daysToMars: number = hoursToMars / 24;
 
 // Part 2: Print Days to Mars
-
+console.log(`${spacecraftName} would take ${daysToMars} days to get to Mars.`)
 
 
 // Code an output statement here (use a template literal):
